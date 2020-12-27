90f315fbab3baa50f78ffe73720460440ad5cc9d
simon987 <me@simon987.net>
2020-12-27 09:23:46 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="parler-api",
+    name="parler_api",
     version="1.0",
     description="Reverse engineered Parler API",
     author="d0nk",
     author_email="",
-    packages=["parler_api"],
+    packages=["parler"],
     install_requires=[
         "requests",
     ]
